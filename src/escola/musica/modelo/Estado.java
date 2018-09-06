d8ab3f31e9fdaaf33db453fc110007b1baf55768
motografa <camilaconsiglio@gmail.com>
2018-09-05 21:31:43 -0300

    Aula 35 - Novo item de menu: Cidades e listagem

--- src/escola/musica/modelo/Estado.java
+++ src/escola/musica/modelo/Estado.java
@@ -1,11 +1,41 @@
 package escola.musica.modelo;
 
 public enum Estado {
-	SP("São Paulo"), //
-	RJ("Rio de Janeiro"), //
-	PR("Paraná"), //
-	SC("Santa Catarina"), //
-	MG("Minas Gerais");//
+//	SP("São Paulo"), //
+//	RJ("Rio de Janeiro"), //
+//	PR("Paraná"), //
+//	SC("Santa Catarina"), //
+//	MG("Minas Gerais");//
+	
+	
+
+	AC("Acre"),//
+	AL("Alagoas"),//
+	AP("Amapá"),//
+	AM("Amazonas"),//
+	BA("Bahia"),//
+	CE("Ceará"),//
+	DFL("Distrito Federal"),//
+	ES("Espírito Santo"),//
+	GO("Goiás"),//
+	MA("Maranhão"),//
+	MT("Mato Grosso"),//
+	MS("Mato Grosso do Sul"),//
+	MG("Minas Gerais"),//
+	PA("Pará"),//
+	PB("Paraíba"),//
+	PR("Paraná"),//
+	PE("Pernambuco"),//
+	PI("Piauí"),//
+	RR("Roraima"),//
+	RO("Rondônia"),//
+	RJ("Rio de Janeiro"),//
+	RN("Rio Grande do Norte"),//
+	RS("Rio Grande do Sul"),//
+	SC("Santa Catarina"),//
+	SP("São Paulo"),//
+	SE("Sergipe"),//
+	TO("Tocantins");
 
 	private Estado(String label) {
 		this.label = label;
